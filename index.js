7ba2bac70eba1889bcf24b08adfdc07bfb77e339
Miguelkabz <kabzmiguel@gmail.com>
2025-09-15 20:22:43 +0300

    math done

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+//Create a variable called random that will generate a random integer greater than 0.
+let random = Math.floor(Math.random() * 10) + 1;
+
+//Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+//Create a variable called max that will return the largest number among the numbers 1, 5, 10, 15, and 20.
+let max = Math.max(1, 5, 10, 15, 20);
+
+
+//
+
+console.log(multiply);
+console.log(random);
+console.log(mod);
+console.log(max);
